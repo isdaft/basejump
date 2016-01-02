c4ac529f047a92e7ff77039e492d661e5969ead0
isdaft <ben.ashby2@gmail.com>
2016-01-02 18:22:07 +0000

    working server.js that requests '/' returns plaintext instruction, and requests '/:query' and throws into variable for later manipulation (no done manip)

--- server.js
+++ server.js
@@ -1,33 +1,19 @@
-'use strict';
-
+var http = require('http');
+var url = require('url');
+var port = process.env.PORT || 8080;
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
 
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
-
-mongoose.connect(process.env.MONGO_URI);
-
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
-
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
-
-var port = process.env.PORT || 8080;
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+app.get('/:query', function (req, res) {
+var parsedUrl = req.params.query;  
+	//parsed URL contains whats in the url passed /
+	res.send(parsedUrl);
+})
+app.get('/', function(req, res){
+	//
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Enter date in UNIX or TIME format in URL to return json');
+})
+app.listen(port, function() {
+console.log('listening on port ' + port)
 });
